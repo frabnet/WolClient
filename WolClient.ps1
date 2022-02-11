b5bbcad4230761dfe3968ee86aef13b01583297b
Frab <frabnet@users.noreply.github.com>
2022-02-11 15:53:35 +0100

    rewrite TestTCPPort function

--- WolClient.ps1
+++ WolClient.ps1
@@ -1,83 +1,26 @@
-function WaitForKey {
-    param (
-        [String] $Msg
-    )
-    Write-Host $Msg 
-    If ( $psISE ) { Pause } else { [void][System.Console]::ReadKey($FALSE) }    
-}
-
-If ( -not ( Test-Path -Path .\WolClientConfig.xml ) )  {
-    Write-Host "Errore: WolClientConfig.xml non trovato. Adattare e rinominare WolClientConfig_Sample.xml"
-    WaitForKey -Msg "Premere un tasto per uscire."
-    Exit
-}
-
-[xml]$configFile = Get-Content -Path .\WolClientConfig.xml
-
-
-###########################
-# FASE1 Verifica internet #
-###########################
-Write-Host -NoNewLine "Verifica connessione internet... "
-if ( Test-Connection -ComputerName "www.google.it" -Quiet -Count 2 ) {
-    Write-Host -ForegroundColor Green "Ok"
-} else {
-    Write-Host -ForegroundColor Green "Errore"
-    Write-Host "Controllare connessione internet e riprovare."
-    WaitForKey -Msg "Premere un tasto per uscire."
-    Exit
-}
-
-
-######################
-# FASE2 Verifica VPN #
-######################
-
-Write-Host -NoNewLine "Verifica connessione VPN..."
-If (-not ( Test-Connection -ComputerName $configFile.Settings.pfSense.Host -Quiet -Count 2 )) {
-    #Ricerca file OVPN
-    $VpnFile = $configFile.Settings.Vpn.VpnFile
-    if ( $VpnFile -eq "AUTO" ) {$VpnFile = (Get-ChildItem -Path $Env:Userprofile\OpenVPN\config\*.ovpn | Select-Object -First 1).Name }
-    #Avvio client OpenVPN    
-    Start-Process -FilePath "$env:programfiles\OpenVPN\bin\openvpn-gui.exe" -ArgumentList "--command connect $VpnFile"
-    While ( -not ( Test-Connection -ComputerName $configFile.Settings.pfSense.Host -Quiet -Count 2 ) ) {        
-        Write-Host -NoNewline "."
-        Sleep -Milliseconds 500
-    }
-}
-Write-Host -ForegroundColor Green " Ok"
-
-
-#####################
-# FASE3 Verifica PC #
-#####################
-function TestConn([string]$srv,$port=135,$timeout=3000,[switch]$verbose){
-    #Alternativa a
-    #Test-NetConnection -ComputerName $computer -Port $rdpport -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationLevel Quiet
-    #con timeout configurabile https://web.archive.org/web/20150405035615/http://poshcode.org/85
-    $ErrorActionPreference = "SilentlyContinue"
-    $tcpclient = new-Object system.Net.Sockets.TcpClient
-    $iar = $tcpclient.BeginConnect($srv,$port,$null,$null)
-    $wait = $iar.AsyncWaitHandle.WaitOne($timeout,$false)
-    if(!$wait) {
-        $tcpclient.Close()
-        if($verbose){Write-Host "Connection Timeout"}
-        Return $false
+Function WaitForKey {
+    Param ($msg, $error=$false)
+    If ($error) {
+        Write-Host -ForegroundColor White -BackgroundColor Red $msg
+        Write-Host "Premere un tasto per uscire."
     } else {
-    $error.Clear()
-    $tcpclient.EndConnect($iar) | out-Null
-    if(!$?){
-        if($verbose){write-host $error[0]};$failed = $true}
-        $tcpclient.Close()
-    }
-    if($failed){return $false}else{return $true}
+        Write-Host $msg
+    }    
+    If ( $psISE ) { Pause } else {  [void][System.Console]::ReadKey($true) }    
 }
-
-
+Function TestTCPPort {
+    Param($address, $port, $timeout=500)
+    $client = New-Object System.Net.Sockets.TcpClient
+    $beginConnect = $client.BeginConnect($address, $port, $null, $null)
+    Start-Sleep -Milliseconds $timeout
+    $Connected = $client.Connected
+    $client.Close()
+    Return $Connected
+}
 Function SendWakeOnLan {
-Write-Host -NoNewline "Invio comando accensione... "
-#Ignora errore certificato self-signed
-add-type @"
+    Write-Host -NoNewline "Invio comando accensione... "
+    #Ignora errore certificato self-signed
+    add-type @"
     using System.Net;
     using System.Security.Cryptography.X509Certificates;
     public class TrustAllCertsPolicy : ICertificatePolicy {
@@ -88,67 +31,96 @@ add-type @"
         }
     }
 "@
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-$pfSenseUrl = "https://$($configFile.Settings.pfSense.Host)"
-$Timeout = 10
-#Pagina iniziale (per token csrf)
-$LoginPage = Invoke-WebRequest -TimeoutSec $Timeout -Uri $pfSenseUrl -SessionVariable Session
-$CsrfToken = $LoginPage.InputFields.FindByName('__csrf_magic').Value
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+    $pfSenseUrl = "https://$($configFile.Settings.pfSense.Host)"
+    $Timeout = 10
+    #Pagina iniziale (per token csrf)
+    $LoginPage = Invoke-WebRequest -TimeoutSec $Timeout -Uri $pfSenseUrl -SessionVariable Session
+    $CsrfToken = $LoginPage.InputFields.FindByName('__csrf_magic').Value
 
-#Login
-$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $configFile.Settings.pfSense.Username, (ConvertTo-SecureString -AsPlainText -Force ($configFile.Settings.pfSense.Password))
-$Data = @{
-	__csrf_magic=$CsrfToken;
-	usernamefld=$Credential.GetNetworkCredential().UserName;
-	passwordfld=$Credential.GetNetworkCredential().Password;
-	login='Login';
+    #Login
+    $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $configFile.Settings.pfSense.Username, (ConvertTo-SecureString -AsPlainText -Force ($configFile.Settings.pfSense.Password))
+    $Data = @{
+	    __csrf_magic=$CsrfToken;
+	    usernamefld=$Credential.GetNetworkCredential().UserName;
+	    passwordfld=$Credential.GetNetworkCredential().Password;
+	    login='Login';
+    }
+    $Result = Invoke-WebRequest -TimeoutSec $Timeout -WebSession $Session -Uri $pfSenseUrl -Method Post -Body $Data 
+    $CsrfToken = $Result.InputFields.FindByName('__csrf_magic').Value
+
+    #Wake on lan
+    $Data = @{
+	    __csrf_magic=$CsrfToken;
+	    if='lan';
+	    mac= $configFile.Settings.Pc.Mac;
+	    Submit='Send';
+    }
+    $Result = Invoke-WebRequest -TimeoutSec $Timeout -WebSession $Session -Uri "${pfSenseUrl}/services_wol.php" -Method Post -Body $Data 
+    $CsrfToken = $Result.InputFields.FindByName('__csrf_magic').Value
+
+    if ($Result.RawContent -like "*Sent magic packet to*") {
+        Write-Host -ForegroundColor Green "Ok"
+    } else {
+        Write-Host -ForegroundColor Red "Errore"
+    }
+
+    #Logout
+    $Data = @{
+        __csrf_magic=$CsrfToken;
+        logout=""
+    }
+    $Result = Invoke-WebRequest -TimeoutSec $Timeout -WebSession $Session -Uri "${pfSenseUrl}/index.php?logout" -Method Post -Body $Data 
+    $CsrfToken = $Result.InputFields.FindByName('__csrf_magic').Value
 }
-$Result = Invoke-WebRequest -TimeoutSec $Timeout -WebSession $Session -Uri $pfSenseUrl -Method Post -Body $Data 
-$CsrfToken = $Result.InputFields.FindByName('__csrf_magic').Value
 
-#Wake on lan
-$Data = @{
-	__csrf_magic=$CsrfToken;
-	if='lan';
-	mac= $configFile.Settings.Pc.Mac;
-	Submit='Send';
+# Read config file
+If ( -not ( Test-Path -Path .\WolClientConfig.xml ) )  {
+    WaitForKey -Msg "ERRORE: WolClientConfig.xml non trovato." -Error $true
+    Exit
 }
-$Result = Invoke-WebRequest -TimeoutSec $Timeout -WebSession $Session -Uri "${pfSenseUrl}/services_wol.php" -Method Post -Body $Data 
-$CsrfToken = $Result.InputFields.FindByName('__csrf_magic').Value
+[xml]$configFile = Get-Content -Path .\WolClientConfig.xml
 
-if ($Result.RawContent -like "*Sent magic packet to*") {
+# Check internet connection (ping www.google.it)
+Write-Host -NoNewLine "Verifica connessione internet... "
+if ( Test-Connection -ComputerName "www.google.it" -Quiet -Count 2 ) {
     Write-Host -ForegroundColor Green "Ok"
 } else {
-    Write-Host -ForegroundColor Red "Errore"
+    Write-Host -ForegroundColor Green "Errore"
+    WaitForKey -Msg "ERRORE: Connessione a internet non funzionante" -Error $true
+    Exit
 }
 
-#Logout
-$Data = @{
-    __csrf_magic=$CsrfToken;
-    logout=""
-}
-$Result = Invoke-WebRequest -TimeoutSec $Timeout -WebSession $Session -Uri "${pfSenseUrl}/index.php?logout" -Method Post -Body $Data 
-$CsrfToken = $Result.InputFields.FindByName('__csrf_magic').Value
-}
-
-Write-Host -NoNewline "Verifica accensione PC..."
-if ( -Not ( TestConn -Srv $configFile.Settings.Pc.Host -Port 3389 -Timeout 600 ) ) {
-    Write-Host -ForegroundColor Red " Spento"
-    WaitForKey -Msg "Premere un tasto per accendere il computer."
-    SendWakeOnLan
-    Write-Host -NoNewLine "Attesa PC"
-    While ( -Not ( TestConn -Srv $configFile.Settings.Pc.Host -Port 3389 -Timeout 600 ) ) {
-        Write-Host -NoNewLine "."
-        Sleep -Seconds 1
+#Check VPN Connection (tcp connect to pfSense:443)
+Write-Host -NoNewLine "Verifica connessione VPN..."
+If (-not ( TestTCPPort -address $configFile.Settings.pfSense.Host -port 443 ) ) {
+    # Search for the VPN file
+    $VpnFile = $configFile.Settings.Vpn.VpnFile
+    if ( $VpnFile -eq "AUTO" ) {$VpnFile = (Get-ChildItem -Path $Env:Userprofile\OpenVPN\config\*.ovpn | Select-Object -First 1).Name }
+    # Launches OpenVPN gui with selected VPN profile
+    Start-Process -FilePath "$env:programfiles\OpenVPN\bin\openvpn-gui.exe" -ArgumentList "--command connect $VpnFile"
+    While ( -not ( TestTCPPort -address $configFile.Settings.pfSense.Host -port 443 ) ) {        
+        Write-Host -NoNewline "."
+        Sleep -Milliseconds 500
     }
 }
 Write-Host -ForegroundColor Green " Ok"
 
+#Check if PC is powered on (tcp connect to pc:3389)
+Write-Host -NoNewline "Verifica accensione PC..."
+if ( -Not ( TestTCPPort -address $configFile.Settings.Pc.Host -Port 3389 ) ) {
+    Write-Host -ForegroundColor Red " Spento"
+    WaitForKey -Msg "Premere un tasto per accendere il computer."
+    SendWakeOnLan
+    Write-Host -NoNewLine "Attesa PC"
+    While ( -Not ( TestTCPPort -address $configFile.Settings.Pc.Host -Port 3389 ) ) {
+        Write-Host -NoNewLine "."
+        Sleep -Milliseconds 500
+    }
+}
+Write-Host -ForegroundColor Green " Ok"
 
-###################
-# FASE4 Avvio RDP #
-###################
-
+# Start RDP Connection
 $RdpContent = "
 screen mode id:i:2
 session bpp:i:16
@@ -195,6 +167,6 @@ use redirection server name:i:0
 rdgiskdcproxy:i:0
 kdcproxyname:s:
 "
-
 $RdpContent | Out-File "rdp.rdp"
+Sleep -Seconds 1
 Start-Process -FilePath "$env:SystemRoot\system32\mstsc.exe" -ArgumentList "rdp.rdp"
